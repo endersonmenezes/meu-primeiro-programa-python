9e540abf66a119e4049189df26ceedc21a87ea36
Enderson Menezes (Mr. Enderson) <endersonster@gmail.com>
2024-03-11 23:17:12 +0000

    teste

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print("Hello World!")
+numero: int = 2
+dinheiro: float = 2.4
+palavra: str = "Palavra"
+listas: list = ["Palavra", palavra, numero]
+dicionarios: dict = {
+    "chave": "valor",
+    "chave_2": numero
+}
+booleano: bool = True
+booleano: bool = False
+tupla: tuple = (palavra, "Imultável")
+
